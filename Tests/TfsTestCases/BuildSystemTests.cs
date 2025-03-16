52cfd152ba95dfc7d9df85a7f99fe4f218274bb6
Jared Shipley <jared.shipley@siemens.com>
2025-03-16 15:07:51 +0100

    add test case for starting build with variables

--- Tests/TfsTestCases/BuildSystemTests.cs
+++ Tests/TfsTestCases/BuildSystemTests.cs
@@ -1,5 +1,6 @@
 using DevOpsMatrix.Core;
 using DevOpsMatrix.Interface;
+using TestConfig;
 
 namespace TfsTestCases
 {
@@ -24,10 +25,10 @@ namespace TfsTestCases
         [OneTimeSetUp] 
         public void SetupFixture()
         {
-            //if (TestSettings.Instance.TestServers.ContainsKey("Chief-PNB"))
-            //    m_settings = TestSettings.Instance.TestServers["Chief-PNB"];
-            //else
-            //    throw new InvalidDataException("Test Settings does not contain an entry for Chief");
+            if (TestSettings.Instance.TestServers.ContainsKey("tfssvr-PNB"))
+                m_settings = TestSettings.Instance.TestServers["tfssvr-PNB"];
+            else
+                throw new InvalidDataException("Test Settings does not contain an entry for tfssvr-PNB");
         }
 
         [Test]
@@ -61,6 +62,25 @@ namespace TfsTestCases
                 Assert.Fail("Failed to retrieve Pipeline PNB_Dev1");
 
             IDevOpsPipelineBuild build = pipe.GetLatestBuild();
+            Assert.That(build != null);
+
+            Assert.That(build.BuildSteps.Count > 0);
+
+            Assert.Pass("Everything worked as expected");
+        }
+
+        [Test]
+        public void LaunchBuildWithParams()
+        {
+            IDevOpsPipeline pipe = m_buildSystem.GetPipeline("PNB_Dev1");
+            if (pipe == null)
+                Assert.Fail("Failed to retrieve Pipeline PNB_Dev1");
+
+            Dictionary<string, string> buildvars = new Dictionary<string, string>();
+            buildvars.Add("ChangeId", "12");
+
+            IDevOpsPipelineBuild build = m_buildSystem.LaunchPipelineBuild(pipe, buildvars);
+
             Assert.That(build != null);
 
             Assert.That(build.BuildSteps.Count > 0);
